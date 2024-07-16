b85a2b79aa4c6583c32042749692e9f36e8c6875
diemaf2004 <mariafernandacalvachemunoz21@gmail.com>
2024-07-16 10:43:05 -0500

    hola

--- database/migrations/2024_07_08_213446_create_destinos_table.php
+++ database/migrations/2024_07_08_213446_create_destinos_table.php
@@ -4,22 +4,30 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDestinosTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('destinos', function (Blueprint $table) {
             $table->id();
             $table->string('codigo')->unique();
-            $table->string('nombre');
+            $table->integer('num_plazas');
+            $table->date('fecha');
             $table->text('otros_datos')->nullable();
+            $table->foreignId('viajero_id')->constrained()->onDelete('cascade');
             $table->timestamps();
+
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('destinos');
     }
-}
-
+};
